33d7f4f95eaf749cf7cbda85db51a0805717eeb1
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-06-23 04:47:25 +0530

    navigation to palce order done

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import {useNavigate} from 'react-router-dom';
 
  export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ import { toast } from "react-toastify";
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const navigate = useNavigate();
 
 
     const addToCart = async (itemId, size) => {
@@ -77,7 +79,7 @@ const getCartAmount =  ()=> {
 }
 
     const value = {
-        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart, getCartCount, updateQuantity, getCartAmount
+        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart, getCartCount, updateQuantity, getCartAmount, navigate
     }
 
     return (
