ef3eaa26cccc954a32f8f8c3e2b4a121d0b173ae
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-06-25 19:53:59 +0530

    admin login authentication done

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
 import  {addProduct, removeProduct, listProducts, singleProduct} from "../controllers/productController.js";
 import upload from "../middlewares/multer.js";
+import adminAuth from "../middlewares/adminAuth.js";
 
 const productRouter = express.Router();
 
-productRouter.post('/add',
+productRouter.post('/add', 
+    adminAuth,
     upload.fields([
         {name:'image1', maxCount:1},
         {name:'image2', maxCount1:1},
@@ -12,7 +14,7 @@ productRouter.post('/add',
         {name:'image4', maxCount1:1},
     ]), addProduct);
 
-productRouter.post('/remove', removeProduct);
+productRouter.post('/remove',adminAuth, removeProduct);
 productRouter.post('/single', singleProduct);
 productRouter.post('/list', listProducts);
 
