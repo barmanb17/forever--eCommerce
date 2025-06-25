7626db3a7e32e3294ae649a707a3bb16f012d10f
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-06-25 18:19:07 +0530

    functionality for products api is created

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -73,8 +73,15 @@ const listProducts = async (req, res) => {
  }
 
  //function for single product view
- const singleProduct = (req, res) => {
-
+ const singleProduct =async (req, res) => {
+    try {
+        const {productId} = req.body
+        const product = await productModel.findById(productId)
+        res.json({success: true, product})
+    } catch (error) {
+                console.log(error)
+        res.json({success: false, message: error.message})  
+    }
  }
 
  export {addProduct, removeProduct, listProducts, singleProduct};
