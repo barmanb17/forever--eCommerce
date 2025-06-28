42e985d362d6bd1a6de2af3169618a4d29c2a16a
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-06-28 23:49:23 +0530

    formatted

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom'
 import { assets } from '../assets/assets'
 
 const Sidebar = () => {
-  return (
-    <div className='w-[18%] min-h-screen border-r-2'>
-        <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
+   return (
+      <div className='w-[18%] min-h-screen border-r-2'>
+         <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
             <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l" to="/add">
                <img className='w-5 h-5' src={assets.add_icon} alt="" />
                <p className='hidden md:block'>Add items</p>
@@ -20,9 +20,9 @@ const Sidebar = () => {
                <img className='w-5 h-5' src={assets.order_icon} alt="" />
                <p className='hidden md:block'>Orders</p>
             </NavLink>
-        </div>
-    </div>
-  )
+         </div>
+      </div>
+   )
 }
 
 export default Sidebar
