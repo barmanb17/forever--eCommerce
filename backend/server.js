e2f59d61426f2421b3f2f507d1a191354c5f6eb1
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-09-10 23:26:12 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -6,22 +6,21 @@ import connectCloudinary from './config/cloudinary.js';
 import router from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
 
-//app config
+
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB()
 connectCloudinary()
 
-//middlewares
 app.use(express.json());
 app.use(cors());
 
-//api endpoints
+
 app.get("/", (req, res) => {
     res.send("API working...")
 })
 
-//routes
+
 app.use("/api/user", router);
 app.use("/api/product", productRouter);
 
